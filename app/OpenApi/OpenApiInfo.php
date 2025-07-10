8fe3df702d33457d77da503b699665836a2f429d
fernando <fernando@juandemariana.org>
2025-07-10 18:02:34 +0200

    Configure API documentation.

---
+++ app/OpenApi/OpenApiInfo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\OpenApi;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="API de Mi Proyecto",
+ *     description="Documentación generada automáticamente con Swagger",
+ *     @OA\Contact(
+ *         name="Soporte",
+ *         email="soporte@miapp.com"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://opensource.org/licenses/MIT"
+ *     )
+ * )
+ */
+class OpenApiInfo {}
