8acb856be59fcab39965a51966d1eeb541fe0800
fernando <fernando@juandemariana.org>
2025-08-13 23:33:47 +0200

    Add admin/logout route.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta personalizada para logout que maneja tanto GET como POST
+Route::match(['GET', 'POST'], '/admin/logout', function () {
+    Auth::logout();
+    session()->invalidate();
+    session()->regenerateToken();
+    
+    return redirect('/admin/login');
+})->name('admin.logout');
