4b55d37c5c1d8fcf44253019a874a9eca0cb3b61
fernando <fernando@juandemariana.org>
2025-08-14 22:09:57 +0200

    Create companies table.

---
+++ database/migrations/2025_08_14_200819_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cif')->unique();
+            $table->string('contact_person');
+            $table->text('company_address');
+            $table->timestamps();
+            
+            // Índices para mejorar el rendimiento
+            $table->index('cif');
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
