b9e364bce38083673e6155bba71dbac126c09df7
fernando <fernando@juandemariana.org>
2025-08-14 22:22:01 +0200

    Add OrganizationRole Filament resource.

---
+++ app/Filament/Resources/OrganizationRoleResource/Pages/CreateOrganizationRole.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\OrganizationRoleResource\Pages;
+
+use App\Filament\Resources\OrganizationRoleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateOrganizationRole extends CreateRecord
+{
+    protected static string $resource = OrganizationRoleResource::class;
+}
