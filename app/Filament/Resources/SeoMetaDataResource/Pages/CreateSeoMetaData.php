b41bf7c5995325618dc05bcccc6f85cb6c9d8b45
fernando <fernando@juandemariana.org>
2025-08-15 15:21:07 +0200

    Add SeoMetaData Filament resource.

---
+++ app/Filament/Resources/SeoMetaDataResource/Pages/CreateSeoMetaData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SeoMetaDataResource\Pages;
+
+use App\Filament\Resources\SeoMetaDataResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateSeoMetaData extends CreateRecord
+{
+    protected static string $resource = SeoMetaDataResource::class;
+}
