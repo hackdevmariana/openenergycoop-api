9e7eb09aa84b4ecd58760464cd088d799e0c7c75
fernando <fernando@juandemariana.org>
2025-08-16 21:51:21 +0200

    Add Municipality Filament resource.

---
+++ app/Filament/Resources/MunicipalityResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MunicipalityResource\Pages;
+use App\Filament\Resources\MunicipalityResource\RelationManagers;
+use App\Models\Municipality;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MunicipalityResource extends Resource
+{
+    protected static ?string $model = Municipality::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
+    
+    protected static ?string $navigationGroup = 'Geografía';
+    
+    protected static ?string $modelLabel = 'Municipio';
+    
+    protected static ?string $pluralModelLabel = 'Municipios';
+    
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('text')
+                    ->columnSpanFull(),
+                Forms\Components\Select::make('province_id')
+                    ->relationship('province', 'name')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('province.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMunicipalities::route('/'),
+            'create' => Pages\CreateMunicipality::route('/create'),
+            'edit' => Pages\EditMunicipality::route('/{record}/edit'),
+        ];
+    }
+}
