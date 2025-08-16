37c15aa00fc82e290523d8e97be6c335843289ed
fernando <fernando@juandemariana.org>
2025-08-16 20:56:22 +0200

    Create regions table.

---
+++ database/migrations/2025_08_16_184530_create_regions_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
+            $table->timestamps();
+
+            // Índices
+            $table->index('slug');
+            $table->index('name');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('regions');
+    }
+};
