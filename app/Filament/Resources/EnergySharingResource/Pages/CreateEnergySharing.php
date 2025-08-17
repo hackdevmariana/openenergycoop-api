faaa08cea30841dc39778854dae0d5fda83d5f70
fernando <fernando@juandemariana.org>
2025-08-17 09:16:53 +0200

    Add EnergySharing Filament resource.

---
+++ app/Filament/Resources/EnergySharingResource/Pages/CreateEnergySharing.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\EnergySharingResource\Pages;
+
+use App\Filament\Resources\EnergySharingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEnergySharing extends CreateRecord
+{
+    protected static string $resource = EnergySharingResource::class;
+}
