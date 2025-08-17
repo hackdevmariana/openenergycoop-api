801be4ca994b152bedf14b52ce471883197cb66a
fernando <fernando@juandemariana.org>
2025-08-17 06:18:55 +0200

    Add EnergyConsuption Filament resource.

---
+++ app/Filament/Resources/EnergyConsumptionResource/Pages/CreateEnergyConsumption.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\EnergyConsumptionResource\Pages;
+
+use App\Filament\Resources\EnergyConsumptionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEnergyConsumption extends CreateRecord
+{
+    protected static string $resource = EnergyConsumptionResource::class;
+}
