8fcbb3281cbe0b68c791e5d707fe8bc6fc2b5027
fernando <fernando@juandemariana.org>
2025-08-22 17:49:16 +0200

    Add EnergyForecast Filament resource.

---
+++ app/Filament/Resources/EnergyForecastResource/Pages/CreateEnergyForecast.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\EnergyForecastResource\Pages;
+
+use App\Filament\Resources\EnergyForecastResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEnergyForecast extends CreateRecord
+{
+    protected static string $resource = EnergyForecastResource::class;
+}
