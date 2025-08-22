ab73fc0006e7972ab04d25d66cac7a83f58ed5e7
fernando <fernando@juandemariana.org>
2025-08-22 17:59:38 +0200

    Add TaxCalculation Filament resource.

---
+++ app/Filament/Resources/TaxCalculationResource/Pages/CreateTaxCalculation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\TaxCalculationResource\Pages;
+
+use App\Filament\Resources\TaxCalculationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTaxCalculation extends CreateRecord
+{
+    protected static string $resource = TaxCalculationResource::class;
+}
