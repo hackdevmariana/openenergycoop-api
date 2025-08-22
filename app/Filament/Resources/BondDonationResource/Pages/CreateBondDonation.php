1a96ad19fa0809bf90e12643e4440933d0a66a64
fernando <fernando@juandemariana.org>
2025-08-22 18:57:23 +0200

    Add BondDonation Filament resource.

---
+++ app/Filament/Resources/BondDonationResource/Pages/CreateBondDonation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BondDonationResource\Pages;
+
+use App\Filament\Resources\BondDonationResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBondDonation extends CreateRecord
+{
+    protected static string $resource = BondDonationResource::class;
+}
