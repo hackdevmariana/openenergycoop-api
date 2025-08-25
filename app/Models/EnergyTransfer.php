b2a65ace37742c6d0e1fb60f17182f8c9c06cbe6
fernando <fernando@juandemariana.org>
2025-08-25 17:46:04 +0200

    Fix EnergyTransfer model.

--- app/Models/EnergyTransfer.php
+++ app/Models/EnergyTransfer.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class EnergyTransfer extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'transfer_number',
