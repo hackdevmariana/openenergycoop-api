92c293a4199225ee74696b588085df23b5448f6c
fernando <fernando@juandemariana.org>
2025-08-25 18:06:09 +0200

    Fix EnergySource Filament resource.

--- app/Filament/Resources/EnergySourceResource.php
+++ app/Filament/Resources/EnergySourceResource.php
@@ -30,7 +30,7 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\TernaryFilter;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class EnergySourceResource extends Resource
 {
@@ -842,10 +842,7 @@ class EnergySourceResource extends Resource
 
     public static function getEloquentQuery(): Builder
     {
-        return parent::getEloquentQuery()
-            ->withoutGlobalScopes([
-                SoftDeletingScope::class,
-            ]);
+        return parent::getEloquentQuery();
     }
 
     public static function getNavigationBadge(): ?string
