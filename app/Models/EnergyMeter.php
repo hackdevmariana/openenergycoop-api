a5ad276b4893ac712f78f523eeab9f9777f08f7c
fernando <fernando@juandemariana.org>
2025-08-25 17:44:53 +0200

    Fix EnergyMeter model.

--- app/Models/EnergyMeter.php
+++ app/Models/EnergyMeter.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class EnergyMeter extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'meter_number',
