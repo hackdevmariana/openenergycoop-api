a3c54abefcb30287a1b1421e888d35605d574d99
fernando <fernando@juandemariana.org>
2025-08-25 18:01:24 +0200

    Fix MaintenanceTask model.

--- app/Models/MaintenanceTask.php
+++ app/Models/MaintenanceTask.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MaintenanceTask extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'title',
