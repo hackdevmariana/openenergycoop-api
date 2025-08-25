6bc60ae1ec360d1c7f495253b00983c5d0714e32
fernando <fernando@juandemariana.org>
2025-08-25 18:11:40 +0200

    Fix EnergyTradingOrder Filament resource.

--- app/Filament/Resources/EnergyTradingOrderResource.php
+++ app/Filament/Resources/EnergyTradingOrderResource.php
@@ -30,7 +30,7 @@ use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\TernaryFilter;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class EnergyTradingOrderResource extends Resource
 {
@@ -787,10 +787,7 @@ class EnergyTradingOrderResource extends Resource
 
     public static function getEloquentQuery(): Builder
     {
-        return parent::getEloquentQuery()
-            ->withoutGlobalScopes([
-                SoftDeletingScope::class,
-            ]);
+        return parent::getEloquentQuery();
     }
 
     public static function getNavigationBadge(): ?string
