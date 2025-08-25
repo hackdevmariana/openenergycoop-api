ef90b49bba1135c2aa0f50931085c7886834b0aa
fernando <fernando@juandemariana.org>
2025-08-25 17:54:09 +0200

    Fix TaxCalculation model.

--- app/Models/TaxCalculation.php
+++ app/Models/TaxCalculation.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class TaxCalculation extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'calculation_number',
