b1e3eadd5e96d7322d8759446ef2503b3c7f29a9
fernando <fernando@juandemariana.org>
2025-08-25 17:47:01 +0200

    Fix EnergyBond model.

--- app/Models/EnergyBond.php
+++ app/Models/EnergyBond.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class EnergyBond extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'bond_number',
