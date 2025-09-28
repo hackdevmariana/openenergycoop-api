589ffba8f7f17bce6d4a6a1ef8af9d57b40c311b
Fernando <fernando@juandemariana.org>
2025-09-28 23:52:06 +0200

    Fix Survey Filament resource.

--- app/Filament/Resources/SurveyResource.php
+++ app/Filament/Resources/SurveyResource.php
@@ -19,7 +19,7 @@ class SurveyResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
     
-    protected static ?string $navigationGroup = 'Encuestas';
+    protected static ?string $navigationGroup = 'Soporte y Encuestas';
     
     protected static ?int $navigationSort = 1;
     
