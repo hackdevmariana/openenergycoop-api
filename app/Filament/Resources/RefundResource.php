ec81f8429a6cfb0c20a7fec8e291a409207d6583
Fernando <fernando@juandemariana.org>
2025-09-28 23:56:41 +0200

    Fix Refund Filament resource.

--- app/Filament/Resources/RefundResource.php
+++ app/Filament/Resources/RefundResource.php
@@ -18,7 +18,7 @@ class RefundResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-arrow-uturn-left';
 
-    protected static ?string $navigationGroup = 'Finanzas';
+    protected static ?string $navigationGroup = 'Finanzas y Contabilidad';
 
     protected static ?string $modelLabel = 'Reembolso';
 
